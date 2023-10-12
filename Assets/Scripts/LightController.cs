6855eb515156ccc329f13827f3cc9752a27e1ca0
LargeKnome <tom@fasham.net.au>
2023-10-13 00:19:51 +1000

    Small extra changes
    Light Occlusion
    Text Messages
    Level layout
    extra decorations
    added tmp

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public int occlusionRange = 20;
+    GameObject[] lights;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = GameObject.FindGameObjectsWithTag("Light");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int n = 0; n < lights.Length; n++)
+        {
+
+            if (Vector3.Distance(gameObject.transform.position, lights[n].transform.position) >= occlusionRange)
+            {
+                lights[n].SetActive(false);
+            }
+            else
+            {
+                lights[n].SetActive(true);
+            }
+        }
+    }
+}
